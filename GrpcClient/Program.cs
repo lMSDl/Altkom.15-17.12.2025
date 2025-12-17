79f26d025dae73bbac42e6516af2ae5bc146fbc8
Paweł <pawel@msd.info.pl>
2025-12-17 13:48:03 +0100

    gRPC strumienie

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,29 +2,105 @@
 using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcService.Protos.PeopleProto;
+using GrpcService.Protos.StreamsProto;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:7074");
-var client = new GrpcService.Protos.PeopleProto.PeopleGrpcService.PeopleGrpcServiceClient(channel);
 
-var people = await client.ReadAsync(new Google.Protobuf.WellKnownTypes.Empty());
+var streamClient = new GrpcService.Protos.StreamsProto.GrpcStream.GrpcStreamClient(channel);
+var downStream = streamClient.FromServer(new Request { Message = "Hello from client" });
 
-foreach (var person in people.Collection)
-{
-    Console.WriteLine( $"Id: {person.Id.Value}, First Name: {person.FirstName}, Last Name: {person.LastName}" );
-}
-Console.ReadLine();
-
-var p1 = await client.ReadByIdAsync(new Id { Value = 44 });
-p1 = await client.ReadByIdAsync(new Id { Value = 144 });
-
-var p2 = await client.ReadByIdOptionalAsync(new Id { Value = 44 });
-p2 = await client.ReadByIdOptionalAsync(new Id { Value = 144 });
-
+var token = new CancellationTokenSource();
+//token.CancelAfter(10000);
+token.CancelAfter(1000);
 try
 {
-    await client.UpdateAsync(new Person { Id = new Id { Value = 144 } });
+    while (await downStream.ResponseStream.MoveNext(token.Token))
+    {
+        Console.WriteLine(downStream.ResponseStream.Current.Message);
+    }
 }
-catch (RpcException ex)
+catch (Exception e)
 {
-    Console.WriteLine(ex.Status.Detail);
+    Console.WriteLine("Streaming cancelled");
 }
+token.Dispose();
+
+
+var upStream = streamClient.FromClient();
+foreach (var letter in "ala ma kota")
+{
+    await upStream.RequestStream.WriteAsync(new Request { Message = letter.ToString() });
+}
+await upStream.RequestStream.CompleteAsync();
+var response = await upStream.ResponseAsync;
+
+Console.WriteLine(response.Message);
+
+
+
+var streams = streamClient.Bidirectional();
+
+_ = Task.Run(async () =>
+{
+    for (int i = 0; i < int.MaxValue; i++)
+    {
+        if (i % 2 == 0)
+        {
+            await streams.RequestStream.WriteAsync(new Request { Message = $"Message {i} from client" });
+        }
+        else
+        {
+            await streams.RequestStream.WriteAsync(new Request { Message = $"Inny komunikat {i} od klienta" });
+        }
+        await Task.Delay(750);
+    }
+});
+
+_ = Task.Run(async () =>
+{
+    try
+    {
+        while (await streams.ResponseStream.MoveNext(CancellationToken.None))
+        {
+            Console.WriteLine(streams.ResponseStream.Current.Message);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Bidirectional streaming cancelled");
+    }
+});
+
+
+Console.ReadLine();
+
+
+
+static async Task Unary(GrpcChannel channel)
+{
+    var client = new GrpcService.Protos.PeopleProto.PeopleGrpcService.PeopleGrpcServiceClient(channel);
+
+    var people = await client.ReadAsync(new Google.Protobuf.WellKnownTypes.Empty());
+
+    foreach (var person in people.Collection)
+    {
+        Console.WriteLine($"Id: {person.Id.Value}, First Name: {person.FirstName}, Last Name: {person.LastName}");
+    }
+    Console.ReadLine();
+
+    var p1 = await client.ReadByIdAsync(new Id { Value = 44 });
+    p1 = await client.ReadByIdAsync(new Id { Value = 144 });
+
+    var p2 = await client.ReadByIdOptionalAsync(new Id { Value = 44 });
+    p2 = await client.ReadByIdOptionalAsync(new Id { Value = 144 });
+
+    try
+    {
+        await client.UpdateAsync(new Person { Id = new Id { Value = 144 } });
+    }
+    catch (RpcException ex)
+    {
+        Console.WriteLine(ex.Status.Detail);
+    }
+}
+
---
+++ GrpcService/Services/StreamService.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using GrpcService.Protos.StreamsProto;
+using System.Text;
+
+namespace GrpcService.Services
+{
+    public class StreamService : GrpcService.Protos.StreamsProto.GrpcStream.GrpcStreamBase
+    {
+        public override async Task FromServer(Request request, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            for (int i = 0; i < int.MaxValue && !context.CancellationToken.IsCancellationRequested; i++)
+            {
+                await responseStream.WriteAsync(new Response { Message = $"Message {i} from server: {request.Message}" });
+                await Task.Delay(1000, context.CancellationToken);
+            }
+
+        }
+
+
+        public override async Task<Response> FromClient(IAsyncStreamReader<Request> requestStream, ServerCallContext context)
+        {
+
+            var response = new Response();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            await foreach (var request in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                stringBuilder.AppendLine(request.Message);
+            }
+
+            response.Message = $"Messages received from client had {stringBuilder.Length} characters in total:\n" + stringBuilder.ToString();
+            return response;
+        }
+
+
+        public override async Task Bidirectional(IAsyncStreamReader<Request> requestStream, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            int counter = 0;
+            await foreach (var request in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                if (counter++ % 3 == 0)
+                {
+                    var response = new Response { Message = $"Echo from server: {request.Message}" };
+                    await responseStream.WriteAsync(response);
+                }
+            }
+        }
+    }
+}
